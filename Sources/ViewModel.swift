8db1f660ab46b5070c7b763656efff11b0f036cb
abdullah-original <08.logins_jumbos@icloud.com>
2026-06-01 20:49:36 +0200

    Simplify View Models

--- Sources/ViewModel.swift
+++ Sources/ViewModel.swift
@@ -1,28 +1,23 @@
 import Foundation
 
+/// Conform to this protocol if you don't want to have a single point of entry for actions like in ``ViewModel`` or ``AsyncViewModel``.
 @MainActor
-public protocol AsyncViewModel<State, Action>: ObservableObject {
+public protocol ExtendableViewModel<State>: ObservableObject {
     associatedtype State: Sendable
-    associatedtype Action: Sendable
     
     var state: State { get }
+}
+
+@MainActor
+public protocol AsyncViewModel<State, Action>: ExtendableViewModel {
+    associatedtype Action: Sendable
 
     func execute(_ action: Action) async
 }
 
 @MainActor
-public protocol ViewModel<State, Action>: ObservableObject {
-    associatedtype State: Sendable
+public protocol ViewModel<State, Action>: ExtendableViewModel {
     associatedtype Action: Sendable
     
-    var state: State { get }
-    
     func execute(_ action: Action)
-}
-
-@MainActor
-public protocol ActionLessViewModel<State>: ObservableObject {
-    associatedtype State: Sendable
-    
-    var state: State { get }
 }
